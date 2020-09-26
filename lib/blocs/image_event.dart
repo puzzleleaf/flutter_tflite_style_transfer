93b77f36433778d3ecb51f617febd98a609edd03
Puzzleleaf <cmtyx2@naver.com>
2020-09-26 14:45:49 +0900

    tflite style transfer - base

--- lib/blocs/image_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'image_bloc.dart';
-
-@freezed
-abstract class ImageEvent with _$ImageEvent {
-  const factory ImageEvent.loadModel() = LoadModel;
-  const factory ImageEvent.loadImage() = LoadImage;
-  const factory ImageEvent.transferImage(String styleImagePath) = TransferImage;
-  const factory ImageEvent.resetImage() = ResetImage;
-}
